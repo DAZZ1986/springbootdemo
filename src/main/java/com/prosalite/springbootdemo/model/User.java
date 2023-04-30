66ce83ac31466d7e88e43c86402ecebd1df5dc5f
unknown <art.murtazov@gmail.com>
2023-04-30 12:18:24 +0300

    first commit

---
+++ src/main/java/com/prosalite/springbootdemo/model/User.java
@@ -0,0 +1,23 @@
+package com.prosalite.springbootdemo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+
+
+
+}
